b22cb55a91c345871b64778bb9e29664d538bf9e
GeekVan <lychallengers@gmail.com>
2020-07-14 20:56:39 +0800

    最短路径DP解法

--- leetcode/code64.java
+++ leetcode/code64.java
@@ -7,18 +7,23 @@ package leetcode;
  */
 public class code64 {
     public int minPathSum(int[][] grid) {
-        return getSum(0,0,grid);
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (i == 0 && j != 0) {
+                    grid[i][j] = grid[i][j] + grid[i][j - 1];
+                } else if (j == 0 && i != 0) {
+                    grid[i][j] = grid[i][j] + grid[i - 1][j];
+                }
+                else if(i==0&&j==0){
+
+                }
+                else {
+                    grid[i][j] = Math.min(grid[i - 1][j], grid[i][j - 1]);
+                }
+
+            }
+        }
+        return grid[grid.length-1][grid[0].length-1];
     }
-    public int getSum(int x,int y,int [][] grid){
-        if(x>=grid.length){
-            return getSum(x-1,y+1,grid);
-        }
-        if(y>=grid[0].length){
-            return getSum(x+1,y-1,grid);
-        }
-        if(x==grid.length-1&&y==grid[0].length-1){
-            return grid[x][y];
-        }
-        return grid[x][y]+Math.min(getSum(x+1,y,grid),getSum(x,y+1,grid));
-    }
+
 }
