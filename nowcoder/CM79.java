2d6077c5401028f65945608402dcd3b4be0b9c93
GeekVan <lychallengers@gmail.com>
2020-07-15 22:38:32 +0800

    中位数问题

--- nowcoder/CM79.java
+++ nowcoder/CM79.java
@@ -11,21 +11,22 @@ public class CM79 {
     public static int[] getMiddle(int[] A, int n) {
         // write code here
         int [] result=new int[n];
-        PriorityQueue<Integer> priorityQueueSmall=new PriorityQueue<>((o1, o2) -> o2-o1);
-        PriorityQueue<Integer> priorityQueueBig = new PriorityQueue<>((o1, o2) -> o1-o2);
+        PriorityQueue<Integer> priorityQueueSmall=new PriorityQueue<>((w1, w2) -> w1-w2);
+        PriorityQueue<Integer> priorityQueueBig = new PriorityQueue<>((o1, o2) -> o2-o1);
         int i=0;
         for(int value:A){
-            if(priorityQueueSmall.size()>priorityQueueBig.size()){
-                priorityQueueSmall.offer(value);
-                int tmp=priorityQueueSmall.poll();
-                priorityQueueBig.add(tmp);
-            }
-            else{
+            if(priorityQueueSmall.size()<priorityQueueBig.size()){
                 priorityQueueBig.add(value);
                 int tmp=priorityQueueBig.poll();
                 priorityQueueSmall.add(tmp);
             }
-            result[i]=priorityQueueSmall.peek();
+            else{
+                priorityQueueSmall.offer(value);
+                int tmp=priorityQueueSmall.poll();
+                priorityQueueBig.add(tmp);
+
+            }
+            result[i]=priorityQueueBig.peek();
             i++;
 
         }
